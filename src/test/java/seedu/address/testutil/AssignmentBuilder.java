ab63defb729d548bddef15da7a297fb77db2b41e
Droffilc13 <cliffordktoh@hotmail.com>
2021-09-27 17:44:34 +0800

    Create AssignmentBuilder.java class

--- src/test/java/seedu/address/testutil/AssignmentBuilder.java
+++ src/test/java/seedu/address/testutil/AssignmentBuilder.java
@@ -1,93 +1,81 @@
 package seedu.address.testutil;
 
-import java.util.HashSet;
-import java.util.Set;
-
+import seedu.address.model.assignment.Assignment;
 import seedu.address.model.assignment.Description;
 import seedu.address.model.assignment.DueDate;
 import seedu.address.model.assignment.Status;
-import seedu.address.model.person.Address;
-import seedu.address.model.person.Email;
-import seedu.address.model.person.Name;
-import seedu.address.model.person.Person;
-import seedu.address.model.person.Phone;
-import seedu.address.model.tag.Tag;
-import seedu.address.model.util.SampleDataUtil;
 
 public class AssignmentBuilder {
-//    public static final String DEFAULT_DESCRIPTION = "Assignment description";
-//    public static final String DEFAULT_DATE = "28/9/2021";
-//    public static final
-//    public static final Status DEFAULT_STATUS = Status.createPendingStatus();
-//
-//    private final Description description = new Description(DEFAULT_DESCRIPTION);
-//    private final DueDate dueDate = new DueDate(DEFAULT_DATE);
-//
-//
-//    /**
-//     * Creates a {@code PersonBuilder} with the default details.
-//     */
-//    public PersonBuilder() {
-//        //name = new Name(DEFAULT_NAME);
-//        //phone = new Phone(DEFAULT_PHONE);
-//        email = new Email(DEFAULT_EMAIL);
-//        address = new Address(DEFAULT_ADDRESS);
-//        tags = new HashSet<>();
-//    }
-//
-//    /**
-//     * Initializes the PersonBuilder with the data of {@code personToCopy}.
-//     */
-//    public PersonBuilder(Person personToCopy) {
-//        name = personToCopy.getName();
-//        phone = personToCopy.getPhone();
-//        email = personToCopy.getEmail();
-//        address = personToCopy.getAddress();
-//        tags = new HashSet<>(personToCopy.getTags());
-//    }
-//
-//    /**
-//     * Sets the {@code Name} of the {@code Person} that we are building.
-//     */
-//    public PersonBuilder withName(String name) {
-//        this.name = new Name(name);
-//        return this;
-//    }
-//
-//    /**
-//     * Parses the {@code tags} into a {@code Set<Tag>} and set it to the {@code Person} that we are building.
-//     */
-//    public PersonBuilder withTags(String ... tags) {
-//        this.tags = SampleDataUtil.getTagSet(tags);
-//        return this;
-//    }
-//
-//    /**
-//     * Sets the {@code Address} of the {@code Person} that we are building.
-//     */
-//    public PersonBuilder withAddress(String address) {
-//        this.address = new Address(address);
-//        return this;
-//    }
-//
-//    /**
-//     * Sets the {@code Phone} of the {@code Person} that we are building.
-//     */
-//    public PersonBuilder withPhone(String phone) {
-//        this.phone = new Phone(phone);
-//        return this;
-//    }
-//
-//    /**
-//     * Sets the {@code Email} of the {@code Person} that we are building.
-//     */
-//    public PersonBuilder withEmail(String email) {
-//        this.email = new Email(email);
-//        return this;
-//    }
-//
-//    public Person build() {
-//        return new Person(name, phone, email, address, tags);
-//    }
+    public static final String DEFAULT_DESCRIPTION = "Assignment description";
+    public static final String DEFAULT_DATE = "28/9/2021";
+    public static final String DEFAULT_TIME = "1800";
+    public static final Status DEFAULT_STATUS = Status.createPendingStatus();
+
+    private Description description;
+    private DueDate dueDate;
+    private Status status;
+
+
+    /**
+     * Creates a {@code AssignmentBuilder} with the default details.
+     */
+    public AssignmentBuilder() {
+        description = new Description(DEFAULT_DESCRIPTION);
+        dueDate = new DueDate(DEFAULT_DATE, DEFAULT_TIME);
+        status = DEFAULT_STATUS;
+    }
+
+    /**
+     * Initializes the AssignmentBuilder with the data of {@code assignmentToCopy}.
+     */
+    public AssignmentBuilder(Assignment assignmentToCopy) {
+        description = assignmentToCopy.getDescription();
+        dueDate = assignmentToCopy.getDueDate();
+        status = assignmentToCopy.getStatus();
+    }
+
+    /**
+     * Sets the {@code Description} of the {@code Assignment} that we are building.
+     */
+    public AssignmentBuilder withDescription(String description) {
+        this.description = new Description(description);
+        return this;
+    }
+
+    /**
+     * Sets the {@code DueDate} of the {@code Assignment} that we are building.
+     */
+    public AssignmentBuilder withDueDate(String dueDate) {
+        this.dueDate = new DueDate(dueDate);
+        return this;
+    }
+
+    /**
+     * Sets the {@code Status} to completed of the {@code Assignment} that we are building.
+     */
+    public AssignmentBuilder withCompletedStatus() {
+        this.status = Status.createCompletedStatus();
+        return this;
+    }
+
+    /**
+     * Sets the {@code Status} to pending of the {@code Assignment} that we are building.
+     */
+    public AssignmentBuilder withCPendingStatus() {
+        this.status = Status.createPendingStatus();
+        return this;
+    }
+
+    /**
+     * Sets the {@code Status} to late of the {@code Assignment} that we are building.
+     */
+    public AssignmentBuilder withLateStatus() {
+        this.status = Status.createLateStatus();
+        return this;
+    }
+
+    public Assignment build() {
+        return new Assignment(description, dueDate, status);
+    }
 
 }
