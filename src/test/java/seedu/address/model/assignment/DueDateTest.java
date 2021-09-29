29b6a76309e348719b172cb5bfbb9fe085421514
qinguorui2001 <qinguorui666@gmail.com>
2021-09-29 22:13:58 +0800

    fix trailing space

--- src/test/java/seedu/address/model/assignment/DueDateTest.java
+++ src/test/java/seedu/address/model/assignment/DueDateTest.java
@@ -7,7 +7,6 @@ import static seedu.address.testutil.Assert.assertThrows;
 import org.junit.jupiter.api.Test;
 
 class DueDateTest {
-  
     @Test
     public void constructor_null_throwsNullPointerException() {
         assertThrows(NullPointerException.class, () -> new DueDate(null));
