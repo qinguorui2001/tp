ac93453fd2a60cc1052d81ce1ebbf8272c3c3f50
weichang18 <e0543893@u.nus.edu>
2021-10-09 00:09:14 +0800

    Add testcases for ShowAssignmentCommnad and ShowAssignmentCommandParser

---
+++ src/test/java/seedu/address/logic/parser/ShowAssignmentCommandParserTest.java
@@ -0,0 +1,26 @@
+package seedu.address.logic.parser;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_PERSON;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.ShowAssignmentCommand;
+
+public class ShowAssignmentCommandParserTest {
+
+    private ShowAssignmentCommandParser parser = new ShowAssignmentCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsDeleteCommand() {
+        assertParseSuccess(parser, "1", new ShowAssignmentCommand(INDEX_FIRST_PERSON));
+    }
+
+    @Test
+    public void parse_invalidArgs_throwsParseException() {
+        assertParseFailure(parser, "a",
+                String.format(MESSAGE_INVALID_COMMAND_FORMAT, ShowAssignmentCommand.MESSAGE_USAGE));
+    }
+}
