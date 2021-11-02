1109b0630e1eb25736859adb23facadfefe9be80
qinguorui2001 <qinguorui666@gmail.com>
2021-11-02 18:08:31 +0800

    fix check style

--- src/test/java/seedu/address/logic/commands/AddPersonCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddPersonCommandTest.java
@@ -23,7 +23,6 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.ReadOnlyAddressBook;
 import seedu.address.model.ReadOnlyUserPrefs;
 import seedu.address.model.UserPrefs;
-import seedu.address.model.VersionedAddressBook;
 import seedu.address.model.assignment.Assignment;
 import seedu.address.model.person.Person;
 import seedu.address.testutil.PersonBuilder;
