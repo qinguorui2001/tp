321e2acfbfa1a134de674ee16db08d5a27e3b283
weichang18 <e0543893@u.nus.edu>
2021-11-07 20:20:20 +0800

    fix checkstyle

--- src/main/java/seedu/address/model/assignment/DueDate.java
+++ src/main/java/seedu/address/model/assignment/DueDate.java
@@ -37,12 +37,12 @@ public class DueDate implements Comparable<DueDate> {
             + YEAR_REGEX;
     public static final String LEAP_YEAR_REGEX = "29[/]0?2[/](19|[2-9][0-9])"
             + "(00|04|08|12|16|20|24|28|32|36|40|44|48|52|56|60|64|68|72|76|80|84|88|92|96)";
-    public static final String DATE_VALIDATION_REGEX = "(^"+ NON_LEAP_YEAR_REGEX + "$)|(^" + LEAP_YEAR_REGEX + "$)";
+    public static final String DATE_VALIDATION_REGEX = "(^" + NON_LEAP_YEAR_REGEX + "$)|(^" + LEAP_YEAR_REGEX + "$)";
 
 
     public static final String TIME_VALIDATION_REGEX = "^(00|[0,1][0-9]|2[0-3])([0-5][0-9])$";
     public static final String DATE_AND_TIME_VALIDATION_REGEX =
-              "(("+ NON_LEAP_YEAR_REGEX + ")|(" + LEAP_YEAR_REGEX + "))(,)"
+              "((" + NON_LEAP_YEAR_REGEX + ")|(" + LEAP_YEAR_REGEX + "))(,)"
                       + "(00|[0,1][0-9]|2[0-3])([0-5][0-9])$";
     public static final String LATEST_TIME_IN_DAY = "2359";
     public static final ArrayList<String> FRIENDLY_COMMANDS = initArrayList();
